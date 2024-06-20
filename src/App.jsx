9dc9525d0a857aeded4c9fa627d7fa00150f12c8
Guilherme Lima <guilhermelimaesilva.p@gmail.com>
2024-06-20 20:13:42 -0300

    Finalizando projeto e Responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 import Card from "./components/Card";
 import fetchData from "./services/api";
+import initialData from "./helpers/initialData";
 
 function App() {
   const [city, setCity] = useState('');
-  const [data, setData] = useState({});
+  const [data, setData] = useState(initialData);
   
   const handleSubmit = (event) => {
     event.preventDefault()
+
     fetchData(city).then((response) => {
       setData(response)
     })
   }
 
   return (
-    <div className="flex flex-col w-full h-screen items-center justify-center ">
+    <div className="flex flex-col w-full h-screen items-center sm:justify-center p-4">
 
 
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={ handleSubmit } className="fixed bottom-0 w-full flex p-4 sm:relative justify-center">
         <input
           type="text"
           placeholder="Cidade"
-          className="p-3 rounded-lg outline-none"
+          className="p-3 rounded-lg outline-none w-full flex-1 sm:max-w-[300px]"
           value={city}
           onChange={({ target: { value } }) => setCity(value)}
           />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import propTypes from 'prop-types'
+import propTypes from 'prop-types';
 
 function Card({ data }) {
 
@@ -11,19 +11,19 @@ function Card({ data }) {
 
     return (
 
-        <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
+        <div className="bg-white p-6 mt-10 rounded-lg shadow-md min-w-[220px]">
         {/* Informações de lugar */}
             <div>
-                <span className="block text-lg font-bold text-slate-700">{location.name}</span>
-                <span className="text-slate-400 text-sm font-medium">`${location.region, ${location.country}}`</span>
+                <span className="block text-lg font-bold text-slate-700"> {location.name} </span>
+                <span className="text-slate-400 text-sm font-medium">{location.region}, {location.country}</span>
             </div>
         {/* Temperatura */}
-            <div className="font-bold text-slate-700 flex mt-4 mb-2">
+            <div className="font-bold text-slate-700 flex justify-center mt-4 mb-2">
                 <span className="text-8xl">{current.temp_c}</span>
                 <span className="text-2xl mt-2">°C</span>
             </div>
         {/* Tipo de tempo */}
-            <div className="text-center">
+            <div className="flex justify-center flex-col items-center">
                 <img src={current.condition.icon} alt="" />
                 <span className="text-slate-700 font-medium">{current.condition.text}</span>
             </div>
